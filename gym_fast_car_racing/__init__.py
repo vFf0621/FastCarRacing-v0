0c60e2575283290bb64210b7e5e7c52ef66997a9
Fei Yu Guan <victorkwan1206@yahoo.com>
2023-08-09 14:26:54 -0400

    First Commit

---
+++ gym_fast_car_racing/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 12:36:38 2023
+
+@author: Fei-y
+"""
+from gymnasium.envs.registration import register
+from gym_fast_car_racing.FastCarRacing import FastCarRacing
+
+print("test")
+
+register(
+    id='FastCarRacing-v0',
+    entry_point='gym_fast_car_racing:FastCarRacing',
+    max_episode_steps=1000000000,
+    reward_threshold=1500
+)
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 12:26:28 2023
+
+@author: Fei-y
+"""
+
+from setuptools import setup
+
+setup(
+    name='gym_fast_car_racing',
+    version='0.0.1',
+    url='https://github.com/vFf0621/FastCarRacing-v0',
+    description='Easy to train CarRacing-v2 Environment',
+    packages=['gym_fast_car_racing'],
+    install_requires=[
+        'gymnasium[Box2d]',
+	'torchvision'
+    ]
+)
+
+
